691665dc922c605015ac625d9bb98c00b46fcbaa
Bünyamin Erol <b3tdeveloper@gmail.com>
2021-12-23 18:14:31 +0300

    Loops
    while foreach

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -44,7 +44,7 @@ namespace Loops
 
             for (int i = 0; i < 10; i++)
             {
-                if (i==4)
+                if (i == 4)
                 {
                     break;
                 }
@@ -59,6 +59,40 @@ namespace Loops
                 }
                 Console.WriteLine(i);
             }
+
+            //While
+            Console.WriteLine("\n*******************\n****** WHİLE ******\n*******************\n");
+            Console.WriteLine("Enter A Number : ");
+            int number = int.Parse(Console.ReadLine());
+            int counter1 = 1;
+            int total1 = 0;
+            while (counter1 <= number)
+            {
+                total1 += counter1;
+                counter1++;
+
+            }
+            Console.WriteLine(total1/number);
+
+
+            // 'a' - 'z' letters
+            Console.WriteLine("\n*********************\n****** LETTERS ******\n*********************\n");
+            char character = 'a';
+            while (character <= 'z')
+            {
+                Console.WriteLine(character);
+                character++;
+            }
+
+            Console.WriteLine("\n*********************\n****** FOREACH ******\n*********************\n");
+
+            string[] cars = { "BMW", "FORD", "NISSAN", "TOYOTA" };
+
+            foreach (var item in cars)
+            {
+                Console.WriteLine(item);
+            }
+
 
             Console.ReadLine();
         }
