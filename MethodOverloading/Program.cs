3cfd48c6ebd9b0bf459b69fbc876704244938829
Bünyamin Erol <b3tdeveloper@gmail.com>
2021-12-25 22:03:28 +0300

    method overloading

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //out paramethers
+            string number1 = "999";
+
+            bool result = int.TryParse(number1, out int outNumber);
+            if (result)
+            {
+                Console.WriteLine("Success");
+                Console.WriteLine(outNumber);
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+
+            Methods methods = new Methods();
+            methods.Sum(4, 5, out int total);
+            Console.WriteLine(total);
+
+            //Method Overloading
+            int integer = 999;
+            methods.PrintTheScreen(Convert.ToString(integer));
+            methods.PrintTheScreen(integer);
+            methods.PrintTheScreen("Erol","tr");
+
+            Console.ReadLine();
+        }
+    }
+
+    class Methods
+    {
+        public void Sum(int a, int b, out int total)
+        {
+            total = a+b;
+        }
+
+        public void PrintTheScreen(string data) 
+        { 
+            Console.WriteLine(data); 
+        }
+        public void PrintTheScreen(int data)
+        {
+            Console.WriteLine(data);
+        }
+        public void PrintTheScreen(string data1, string data2)
+        {
+            Console.WriteLine(data1+data2);
+        }
+    }
+}
