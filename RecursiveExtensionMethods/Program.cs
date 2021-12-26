2f17d1c261e3923593e7c61f8fd41524e8a26b50
Bünyamin Erol <b3tdeveloper@gmail.com>
2021-12-26 23:42:13 +0300

    Recursive Extension Methods
    first

---
+++ RecursiveExtensionMethods/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveExtensionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Recursive 
+            //3^4 = 3*3*3*3
+
+            int result = 1;
+            for (int i = 1; i < 5; i++)
+            {
+                result *= 3;
+
+            }
+            Console.WriteLine(result);
+
+            Operations operations = new Operations();
+            Console.WriteLine(operations.Expo(3, 4));
+
+            //Extension Methods
+            string str = "Erol Turkkan";
+            bool result1 = str.CheckSpaces();
+            Console.WriteLine(result1);
+            if (result1)
+            {
+                Console.WriteLine(str.RemoveWhiteSpaces());
+            }
+            Console.WriteLine(str.MakeUpperCase());
+            Console.WriteLine(str.MakeLowerCase());
+
+            int[] array1 = { 9, 2, 5, 6, 7, 4, 5, 4, 0};
+            array1.SortArray();
+            array1.PrintTheScreen();
+
+            int number = 5;
+            Console.WriteLine(number.IsEven());
+
+            Console.WriteLine(str.GetFirstCharacter());
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Operations
+    {
+        public int Expo(int number1, int expo)
+        {
+            if (expo < 2)
+            {
+                return number1;
+            }
+            return Expo(number1, expo - 1) * number1;
+            //Expo(3,4)
+            //Expo(3,3) * 3
+            //Expo(3,2) * 3 * 3;
+            //Expo(3,1) * 3 * 3 * 3;
+            //3 * 3 * 3 * 3 = 3^4;
+        }
+    }
+
+    public static class Extension
+    {
+        public static bool CheckSpaces(this string param)
+        {
+            return param.Contains(" ");
+        }
+
+        public static string RemoveWhiteSpaces(this string param)
+        {
+            string[] array = param.Split(' ');
+            return string.Join("*", array);
+        }
+
+
+        public static string MakeUpperCase(this string param)
+        {
+            return param.ToUpper();
+        }
+
+        public static string MakeLowerCase(this string param)
+        {
+            return param.ToLower();
+        }
+
+        public static int[] SortArray(this int[] param)
+        {
+            Array.Sort(param);
+            return param;
+        }
+
+        public static void PrintTheScreen(this int[] param)
+        {
+            foreach (int item in param)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static bool IsEven(this int param)
+        {
+            return (param % 2 == 0);
+        }
+
+        public static string GetFirstCharacter(this string param)
+        {
+            return param.Substring(0, 1);
+        }
+
+    }
+}
---
+++ RecursiveExtensionMethods/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RecursiveExtensionMethods")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RecursiveExtensionMethods")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("42ad35b1-58d5-4310-b9fc-c11474d96179")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
